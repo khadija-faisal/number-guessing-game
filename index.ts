286d332b2b3552e31eab466446de23737293fec1
Khadija_Faisal <khadijamughal1119@gmail.com>
2024-03-23 01:36:52 +0500

    CLI-Number-Guessing-Game

---
+++ index.ts
@@ -0,0 +1,17 @@
+#!/usr/bin/env node 
+
+import inquirer from "inquirer";
+const randomNum = Math.floor(Math.random() * 6 + 1);
+
+const answer = await inquirer.prompt([
+    {
+        name : "userGuessNum",
+        type : "number",
+        message : "Guess the number between 1-6 :,"    }
+]);
+
+if (answer.userGuessNum == randomNum){
+    console.log("Congratulation, you guessed right number ! ");
+} else {
+    console.log("you Guess wrong number, guess again !");
+}
